987c66f741d72c61706054d8394ba5e762fa83d7
Shawn Caeiro <shawncaeiro@gmail.com>
2015-09-01 09:45:52 -0500

    Remodified wsgi

--- msitracker/wsgi.py
+++ msitracker/wsgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msitracker.settings")
 
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
